1a53ead447d700211bc154b36aed8efd28fd770f
kratospro <88933775+kratospro@users.noreply.github.com>
2021-08-14 05:40:45 -0800

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-bot_token: '',// bot token
-admin: , // bot admin
-currency: '',// bot currency 
-bot_name:'',// bot username without @
-mongoLink:''// mongodb cluster link
-cb_account_id: ''// coinbase acount id
-}
