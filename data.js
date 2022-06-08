1e1da00c69ac0bfdb00e6aed3bcdaee0fbf20106
KratosProject <86033620+KratosProject@users.noreply.github.com>
2022-06-08 16:42:48 +0530

    Update data.js

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ bot_token: '1787236350:AAHvTsGyndetBzbO1obwImRSCOZAHI7zW0o',// bot token
 admin: [1879939671], // bot admin
 currency: 'SHIB',// bot currency 
 bot_name:'santa_cloudbot',// bot username without @
-mongoLink:'mongodb+srv://harshit0987:C9SMpQENcTh4XmZa@cluster0.byr1e.mongodb.net/?retryWrites=true&w=majority'// mongodb cluster link
+mongoLink:'mongodb+srv://harshit:Fqs3eyOblEUyjpSP@cluster0.p2zep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'// mongodb cluster link
 
 
 
