cb072ca78acbf7525f56f4b82b7a371b0773c6ae
KratosProject <86033620+KratosProject@users.noreply.github.com>
2022-06-08 10:57:08 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,2 +1,2 @@
-module.exports ={ bot_token: '1787236350:AAHvTsGyndetBzbO1obwImRSCOZAHI7zW0o',// bot token admin: [1879939671], // bot admin currency: 'SHIB',// bot currency bot_name:'santa_cloudbot',// bot username without @ mongoLink:'mongodb+srv://harshit0987:Harshit@1234@cluster0.byr1e.mongodb.net/?retryWrites=true&w=majority'// mongodb cluster link ' } 
+module.exports ={ bot_token: '1787236350:AAHvTsGyndetBzbO1obwImRSCOZAHI7zW0o',// bot token admin: [1879939671], // bot admin currency: 'SHIB',// bot currency bot_name:'santa_cloudbot',// bot username without @ mongoLink:'mongodb+srv://harshit0987:yTK6XiZXRQf20xV5@cluster0.byr1e.mongodb.net/?retryWrites=true&w=majority'// mongodb cluster link ' } 
 
