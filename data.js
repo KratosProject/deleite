486e10ca9ed89e02703126a6baef59c6febfa3fb
KratosProject <86033620+KratosProject@users.noreply.github.com>
2022-06-14 21:21:22 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,15 @@
  
 
 
+
+
+
 module.exports ={
 bot_token: '1787236350:AAHvTsGyndetBzbO1obwImRSCOZAHI7zW0o',// bot token
 admin: [1879939671], // bot admin
 currency: 'SHIB',// bot currency 
 bot_name:'santa_cloudbot',// bot username without @
 mongoLink:'mongodb+srv://harshit:harshit1@cluster0.ylhn8z5.mongodb.net/?retryWrites=true&w=majority'// mongodb cluster link
-
-
-
+cb_account_id: 'cea9912d-bb3e-5078-aa5c-88fed0389533'// coinbase acount id
 }
+
